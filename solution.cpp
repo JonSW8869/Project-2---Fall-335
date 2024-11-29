bfd4084a4ac1c8d7986d1d7dad9287d522e0beee
JonSW8869 <143462653+JonSW8869@users.noreply.github.com>
2024-11-29 15:38:55 -0500

    j

--- solution.cpp
+++ solution.cpp
@@ -7,10 +7,31 @@
 // You must declare them "inline" | declare & implement them at the top of this file, before query()
 // Below query(), implement and document all methods declared in FileTrie.hpp
 
-// File *searchFile(Node *root)
-// {
-// }
+void queryHelper(Node *node, size_t min, size_t max, std::vector<File *> &result)
+{
+    if (!node)
+        return;
 
+    size_t currentSize = node->files_.front()->getSize();
+
+    // Traverse left subtree if the minimum range is less than the current size
+    if (min < currentSize)
+    {
+        queryHelper(node->left_, min, max, result);
+    }
+
+    // Add files in the current node if they fall within the range
+    if (min <= currentSize && currentSize <= max)
+    {
+        result.insert(result.end(), node->files_.begin(), node->files_.end());
+    }
+
+    // Traverse right subtree if the maximum range is greater than the current size
+    if (max > currentSize)
+    {
+        queryHelper(node->right_, min, max, result);
+    }
+}
 /**
  * @brief Retrieves all files in the FileAVL whose file sizes are within [min, max]
  *
@@ -24,12 +45,11 @@ std::vector<File *> FileAVL::query(size_t min, size_t max)
 {
     // search the tree and push the nodes that have a size inbetween min and max into the vector
     std::vector<File *> result = {};
-    // if (searchFile(root_)->getSize() > min && searchFile(root_)->getSize() < max)
-    // {
-    //     result.push_back(searchFile(root_));
-    // }
-
-    // root_
+    if (min > max)
+    {
+        std::swap(min, max);
+    }
+    queryHelper(root_, min, max, result);
     //  Your code here.
 
     return result;
@@ -40,6 +60,11 @@ std::vector<File *> FileAVL::query(size_t min, size_t max)
 void FileTrie::addFile(File *f)
 {
     // convert to lowercase
+    // FileTrieNode *ptr = head;
+    // std::transform(f->getName().begin(), f->getName().end(), f->getName().begin(), ::towlower);
+    // for (char c : f->getName())
+    // {
+    // }
 }
 
 std::unordered_set<File *> FileTrie::getFilesWithPrefix(const std::string &prefix) const
